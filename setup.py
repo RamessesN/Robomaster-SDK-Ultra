1b0bbf80579a343d573a9abdd1aa80ed81a3fbb3
RamessesN <stanley_zhao0113@outlook.com>
2025-09-06 14:31:39 +0800

    [stanley] fix a bug

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     python_requires = ">=3.7",
     install_requires = [
         "netifaces>=0.11.0",
-        "netaddr>=1.3.0"
+        "netaddr>=1.3.0",
         "scipy>=1.15.0"
     ]
 )
